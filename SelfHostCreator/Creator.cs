558a9e31d2b13d4f1b6460163d88fab7cd4b449d
Olga Lozovik <lozovikolga@gmail.com>
2016-09-13 13:02:14 +0300

    Added SelfHostCreator

---
+++ SelfHostCreator/Creator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WcfToDoService;
+
+namespace SelfHostCreator
+{
+    public class Creator
+    {
+        private static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/toDoService");
+
+            using (ServiceHost host = new ServiceHost(typeof (ToDoService), baseAddress))
+            {
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior
+                {
+                    HttpGetEnabled = true,
+                    MetadataExporter = {PolicyVersion = PolicyVersion.Policy15}
+                };
+                host.Description.Behaviors.Add(smb);
+
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
