9fbb70783f7a16afa8e63cbeb42ec6c2d74fd3ec
Natalia Vladimirova <dochka.tankista@gmail.com>
2016-09-13 12:29:59 +0300

    Add initial implementation of wcf service.

---
+++ WcfToDoService/IToDoService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfToDoService
+{
+    [ServiceContract]
+    public interface IToDoService
+    {
+        [OperationContract]
+        string GetItems(int userId);
+
+        [OperationContract]
+        string CreateItem(ToDoMessage item);
+
+        [OperationContract]
+        string UpdateItem(ToDoMessage item);
+
+        [OperationContract]
+        string DeleteItem(int userId);
+    }
+}
---
+++ WcfToDoService/ToDoMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfToDoService
+{
+    [DataContract]
+    public class ToDoMessage
+    {
+        [DataMember]
+        public int ToDoId { get; set; }
+        
+        [DataMember]
+        public int UserId { get; set; }
+
+        [DataMember]
+        public bool IsCompleted { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
